04fc7ec4c8d8c31427f348208e2cdd672acad55e
Ajamal Khan <13559558+khan-ajamal@users.noreply.github.com>
2021-05-23 11:15:46 +0530

    Add project card

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -1,7 +1,26 @@
 import React from "react"
 
+import Card from "./card"
+
 const Projects = () => {
-  return <div>Projects</div>
+  const projects = [
+    {
+      title: "Sorting Visualizer",
+      description:
+        "A platform to visualize how various sorting algorithm like bubble, merge, quick sorts works",
+      url: "https://khan-ajamal.github.io/sorting-visualizer/",
+    },
+  ]
+  return (
+    <div className="container px-8 mb-16">
+      <h2 className="text-3xl mt-8 mb-4">Projects</h2>
+      <ul className="flex flex-wrap">
+        {projects.map(project => (
+          <Card {...project} key={project} />
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default Projects
--- src/layout/header.js
+++ src/layout/header.js
@@ -17,12 +17,12 @@ const Header = () => {
   `)
 
   return (
-    <header className="absolute w-screen bg-transparent px-4 h-20">
+    <header className="absolute w-screen bg-transparent px-8 h-20">
       <div className="h-full w-full bg-transparent flex justify-between items-center">
         <div className="w-12 h-12">
           <Img fixed={logo.childImageSharp.fixed} alt="Ajamal's Website" />
         </div>
-        <div className="w-full h-full text-white flex justify-end items-center px-8">
+        <div className="w-full h-full text-white flex justify-end items-center ml-8">
           <ul>
             <li className="text-xl">
               <Link to="#projects">Projects</Link>
